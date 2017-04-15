584d76ee3e9c14a45797954330936ec6d5a02174
Kelan Riley <kelan.riley@ndsu.edu>
2017-04-14 20:28:19 -0500

    No new keyword needed to allocate memory for an instance of a class. Interesting.

--- src/experiments/classes.cpp
+++ src/experiments/classes.cpp
@@ -1,5 +1,29 @@
 #include <stdio.h>
 
+class Fraction {
+
+private:
+  int numerator;
+  int denominator;
+
+public:
+  int getDenominator() {
+    return denominator;
+  }
+  int getNumerator() {
+    return numerator;
+  }
+
+  void setNumerator(int n) {
+    numerator = n;
+  }
+
+  void setDenominator(int d) {
+    denominator = d;
+  }
+};
+
 int main(void) {
+  Fraction f;
   printf("Expermenting with classes in C++.");
 }
