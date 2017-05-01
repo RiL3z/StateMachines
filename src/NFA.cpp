d7e89fc57bc66bc7b04f166ea00576f16dcafb76
Kelan Riley <kelan.riley@ndsu.edu>
2017-05-01 03:18:02 -0500

    Making some real progress now!

--- src/NFA.cpp
+++ src/NFA.cpp
@@ -3,8 +3,6 @@
 #include <pthread.h>
 #include <iostream>
 
-static void *processString(void* arg);
-
 using namespace std;
 
 class NFA {
@@ -25,6 +23,14 @@ private:
       retString += inputSymbol;
       retString += "->" + nextState + ")";
       return retString;
+    }
+
+    string getNextState() {
+      return nextState;
+    }
+
+    char getInputSymbol() {
+      return inputSymbol;
     }
   };
   //Private class that represents a state object.
@@ -113,14 +119,45 @@ public:
     }
   }
 
+  vector<string> getStatesForTransition(string stateName, char c) {
+    vector<string> states;
+    //check for start state
+    if(startStateAdded) {
+      if(startState.getName() == stateName) {
+        vector<Transition> transitions = startState.getTransitions();
+        for(int i = 0; i < transitions.size(); i ++) {
+          if(transitions[i].getInputSymbol() == c)
+          states.push_back(transitions[i].getNextState());
+        }
+      }
+    }
+    //then for normal states
+    //then for final states
+    return states;
+  }
+
   void addFinalState(string label) {
     State s = State(label);
     finalStates.push_back(s);
   }
 
+  bool isFinalState(string label) {
+    for(int i = 0; i < finalStates.size(); i ++) {
+      if(label == finalStates[i].getName()) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
   void addStartState(string label) {
     startState = State(label);
     startStateAdded = true;
+  }
+
+  string getStartState() {
+    return startState.getName();
   }
 
   void setLambda(char lambdaLabel) {
@@ -167,26 +204,52 @@ struct arg {
   string exp;
 };
 
+//shared variable that all threads are working to change to
+//true.
+bool accepted = false;
+
 void *processString(void *bundle) {
+  //Reset for every call.
+  accepted = false;
   //Keep track of where this thread is in the reading process.
-  //struct arg data = *((struct arg *) bundle);
-  //string exp = data.exp;
-  //NFA nfa = data.nfa;
-  int readPosition = 0;
+  struct arg data = *((struct arg *) bundle);
+  string exp = data.exp;
+  NFA nfa = data.nfa;
+  string currentState = nfa.getStartState();
+  //Check for e moves first
+
+  //Then check to see if the character read has any transitions
+  //defined for it.
+  for(int i = 0; i < exp.length(); i ++) {
+    vector<string> nextStates = nfa.getStatesForTransition(currentState, exp[i]);
+    if(nextStates.size() != 0) {
+      if(nextStates.size() == 1) {
+        currentState = nextStates[0];
+      }
+      else {
+
+      }
+    }
+    else {
+      return NULL;
+    }
+  }
+
+  if(nfa.isFinalState(currentState)) {
+    accepted = true;
+  }
+
   pthread_exit(NULL);
-  //cout << exp;
 }
 
 bool test(NFA nfa, string expression) {
   pthread_t thread;
-  cout << "here";
   struct arg bundle = {nfa, expression};
   int t = 0;
-  pthread_create(&thread, NULL, processString, (void*) &t);
-  cout << "here";
-  pthread_exit(NULL);
-  cout << "here";
-  return false;
+  pthread_create(&thread, NULL, processString, (void*) &bundle);
+
+  pthread_join(thread, NULL);
+  return accepted;
 }
 
 int main() {
@@ -195,7 +258,8 @@ int main() {
   nfa.addTransition("q1",'a', "q1");
   nfa.addFinalState("q1");
   string nfaString = nfa.toString();
-  string testString = "kelan";
-  test(nfa, testString);
-  cout << nfaString;
+  bool inLanguage = test(nfa, "kelan");
+  bool inLanguage2 = test(nfa, "aaaab");
+  cout << boolalpha;
+  cout << inLanguage2;
 }
